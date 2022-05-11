eef8cb0d7300819e01a2b405ef9e63356a9b23d8
CommandMaker <command.maker83@gmail.com>
2022-05-11 13:51:48 +0200

    ui: petits fixs

--- resources/views/components/header/_header.blade.php
+++ resources/views/components/header/_header.blade.php
@@ -1,6 +1,6 @@
 <header class="app-header">
     <nav class="app-header__navbar">
-        <a href="#" class="app-header__title-link"><img src="/images/patrimag-logo.png" alt="Logo Patri-Mag"></a>
+        <a href="{{ route('index.index') }}" class="app-header__title-link"><img src="/images/patrimag-logo.png" alt="Logo Patri-Mag"></a>
         <div class="app-header__links-container">
             <a href="#" class="app-header__link">Nos articles</a>
             <a href="#" class="app-header__link">Nous contacter</a>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [IndexController::class, 'index']);
+Route::get('/', [IndexController::class, 'index'])->name('index.index');
