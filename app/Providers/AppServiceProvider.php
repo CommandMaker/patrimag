0365d9edd2242c22a4704053be7530940bbe1646
CommandMaker <command.maker83@gmail.com>
2022-05-14 22:32:02 +0200

    env: add cocur/slugify to project & service container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Cocur\Slugify\Slugify;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::defaultView('templates.pagination');
+
+        $this->app->bind(Slugify::class, Slugify::class);
     }
 }
