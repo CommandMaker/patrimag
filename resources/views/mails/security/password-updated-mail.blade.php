6c75c6b4a861f5ef1e111aaf3bfa2905c86562ce
CommandMaker <command.maker83@gmail.com>
2022-06-28 14:18:14 +0200

    fix: add link to reset passwd in security notif mail

--- resources/views/mails/security/password-updated-mail.blade.php
+++ resources/views/mails/security/password-updated-mail.blade.php
@@ -1,5 +1,5 @@
 <p>Bonjour {{ $user->name }}</p>
 
-<p>Votre mot de passe Patri-Mag a été modifié. Si vous n'êtes pas a l'origine de cette modification, vous pouvez le réinitialiser.</p>
+<p>Votre mot de passe Patri-Mag a été modifié. Si vous n'êtes pas a l'origine de cette modification, vous pouvez le <a href="{{ route('security.password-reset-view') }}">réinitialiser</a>.</p>
 
 <p>Cordialement <br>L'équipe des comptes Patri-Mag</p>
