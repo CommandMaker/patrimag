3b51fb15223d0883e657e0d3e328015e7092cde4
CommandMaker <command.maker83@gmail.com>
2022-08-11 18:44:00 +0200

    fix: toolbar for md editor in article edit

--- resources/js/admin/admin-article-edit.js
+++ resources/js/admin/admin-article-edit.js
@@ -3,5 +3,55 @@ import EasyMDE from 'easymde';
 const editor = new EasyMDE({
     element: document.querySelector('#admin-content'),
     spellChecker: false,
-    showIcons: ['strikethrough', 'code', 'table', 'heading-bigger', 'heading-smaller', 'clean-block', 'horizontal-rule'],
+    toolbar: [
+        'bold',
+        'italic',
+        'strikethrough', 
+        'heading',
+        'heading-bigger', 
+        'heading-smaller', 
+        '|',
+        'code',
+        'quote',
+        {
+            name: 'sup',
+            action: (editor) => {
+                const cm = editor.codemirror;
+                let output = '';
+                const selectedText = cm.getSelection() || '';
+
+                output = `<sup>${selectedText}</sup>`;
+                cm.replaceSelection(output);
+            },
+            className: 'fa fa-superscript',
+            title: 'Exposant'
+        },
+        {
+            name: 'ind',
+            action: (editor) => {
+                const cm = editor.codemirror;
+                let output = '';
+                const selectedText = cm.getSelection() || '';
+
+                output = `<sub>${selectedText}</sub>`;
+                cm.replaceSelection(output);
+            },
+            className: 'fa fa-subscript',
+            title: 'Indice'
+        },
+        'unordered-list',
+        'ordered-list',
+        'clean-block',
+        '|',
+        'link',
+        'image',
+        'table',
+        'horizontal-rule',
+        '|',
+        'preview',
+        'side-by-side',
+        'fullscreen',
+        '|',
+        'guide'
+    ],
 });
