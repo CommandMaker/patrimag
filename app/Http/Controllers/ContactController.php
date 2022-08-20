80a2fa43f600daf80ede21423bf497c456c93e7a
CommandMaker <command.maker83@gmail.com>
2022-08-20 17:35:05 +0200

    refactor: larastan & pint

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,11 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-
     public function view(): View
     {
         return view('pages.contact.contact-view');
